33585f56d22f163d97c0bfe7e9fcf41154e2f950
idith almog <almog.idith@gmail.com>
2020-05-04 22:05:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,6 +5,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+      BRANDS << brand
   end
 
   def cobble
@@ -12,9 +13,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand= (brand)
-
-    BRANDS << brand
-  end
-
+  
 end
